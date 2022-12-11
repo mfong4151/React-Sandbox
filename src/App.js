5638a03d26c901b6cf37150f8111f6316613f9db
mfong41564x <mfong415@gmail.com>
2022-12-11 14:12:16 -0800

    Stuff commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffectDemo} from './hooks/useEffect'
+import UseEffectDemo from './hooks/useEffect'
 import UseStateDemo from './hooks/useState'
 
 function App() {
   return (
-    <UseStateDemo />
+    <UseEffectDemo />
   );
 }
 
--- src/hooks/useEffect.jsx
+++ src/hooks/useEffect.jsx
@@ -1,7 +1,63 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import './hooks.css'
+
+const UseEffectDemo1 = ()=>{
+    
+    const[resourceType, setResourceType] = useState('posts');
 
 
-const useEffectDemo = ()=>{
+//    //general example
+//    useEffect(()=>{
+//        console.log('render') //this only runs when the thing in the bottom changes
+//    }, [resourceType]) //Use effect takes two arguments, 
+//                        //to make sure it never changes leave the array empty
+//
 
+    useEffect(()=> {
+
+
+    }, resourceType)
+
+    return(
+        <>
+            <div>
+                <button onClick={() => setResourceType('posts')}>Posts</button>
+                <button onClick={() => setResourceType('users')}>Users</button>
+                <button onClick={() => setResourceType('comments')}>Comments</button>
+
+            </div>
+            <h1>{resourceType}</h1>
+        </>
+
+    )
 
 };
+
+const UseEffectDemo2 = ()=>{
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = ()=>{ 
+        setWindowWidth(window.innerWidth)
+    }
+
+    //What does useEffect do? 
+    //By using this Hook, you tell React that your component needs to do something after render. 
+    //React will remember the function you passed (we’ll refer to it as our “effect”), and call it later after performing the DOM updates. //In this effect, we set the document title, but we could also perform data fetching or call some other imperative API.
+
+    useEffect(()=>{
+        window.addEventListener('resize', handleResize)
+
+        return() => {   //adding a removeEventListener prevents multiple event listeners from being added
+            window.removeEventListener('resize', handleResize)  
+        }
+    }, [])
+
+    return(
+        <div>{windowWidth}</div>
+
+    )
+
+
+}
+
+export {UseEffectDemo1, UseEffectDemo2};
